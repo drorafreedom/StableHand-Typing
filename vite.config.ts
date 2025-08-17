7fd334047a769803772ddee2f1f149a140d16f52
drorafreedom <drora@caltech.edu>
2025-08-17 04:48:05 -0700

    first time login  enroll MFA  working  also the Enroll Phone MOdal is updated to match the count down of login.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',                 // IMPORTANT for Firebase
+  server: {
+    host: '127.0.0.1',
+    port: 1583,
+  },
   build: { outDir: 'dist' },
 });
 
